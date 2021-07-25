da730629cc7eeb6512830a9593be7180d668323c
Andrew Wong <andrew.j.wong@outlook.com>
2021-07-25 21:48:23 +1000

    Cleanup

--- android/app/src/main/java/com/example/andi/MainActivity.java
+++ android/app/src/main/java/com/example/andi/MainActivity.java
@@ -1,69 +1,6 @@
 package com.example.andi;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodChannel;
-
-//
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
-
-//
-import android.net.nsd.NsdManager;
-
-//
 
 public class MainActivity extends FlutterActivity {
-  private static final String CHANNEL = "samples.flutter.dev/battery";
-  private static NsdManager m_nsdManager = null;
-
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    super.configureFlutterEngine(flutterEngine);
-    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-        .setMethodCallHandler((call, result) -> {
-          // Note: this method is invoked on the main thread.
-          switch (call.method) {
-          case "initNSD":
-            if (m_nsdManager == null) m_nsdManager = (NsdManager) getSystemService(NSD_SERVICE);
-            result.success(null);
-            break;
-          case "getBatteryLevel":
-            int batteryLevel = getBatteryLevel();
-
-            if (batteryLevel != -1) {
-              result.success(batteryLevel);
-            } else {
-              result.error("UNAVAILABLE", "Battery level not available.", null);
-            }
-            result.notImplemented();
-
-            break;
-          default:
-            
-            result.notImplemented();
-          }
-        });
-  }
-
-  private int getBatteryLevel() {
-    int batteryLevel = -1;
-    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
-      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-    } else {
-      Intent intent = new ContextWrapper(getApplicationContext()).registerReceiver(null,
-          new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100)
-          / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-    }
-
-    return batteryLevel;
-  }
 }
