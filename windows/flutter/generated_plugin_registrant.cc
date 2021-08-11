53b325e2f9d0f574e52841a8ee3a6145c6021811
Andrew Wong <andrew.j.wong@outlook.com>
2021-08-12 03:33:21 +1000

    touch

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,6 +4,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_ndi/flutter_ndi_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterNdiPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterNdiPlugin"));
 }
